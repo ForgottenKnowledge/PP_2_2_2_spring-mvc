dcd6c668a405259fef1aeecf4c32052cf56db09a
GoodGreenCucumber <igor_pm1632@mail.ru>
2022-05-14 17:48:03 +0400

    Initial commit

---
+++ src/main/java/web/controller/CarController.java
@@ -0,0 +1,32 @@
+package web.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.ArrayList;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestParam;
+import web.model.Car;
+import web.service.CarServiceImpl;
+
+import java.util.List;
+
+@Controller
+public class CarController {
+
+    @GetMapping(value = "/cars")
+    public String viewCars(@RequestParam (defaultValue = "5") int count, ModelMap model) {
+
+        List<Car> allCars = new ArrayList<>();
+        allCars.add(new Car("Red","Mazda",111));
+        allCars.add(new Car("Green","Renault",222));
+        allCars.add(new Car("Blue","BMW",333));
+        allCars.add(new Car("White","Mercedes",444));
+        allCars.add(new Car("Black","Audi",555));
+
+        CarServiceImpl carService = new CarServiceImpl();
+        List<Car> cars = carService.getCarsList(allCars, count);
+        model.addAttribute("cars", cars);
+        return "cars";
+    }
+}
---
+++ src/main/java/web/service/CarServiceImpl.java
@@ -0,0 +1,13 @@
+package web.service;
+
+import web.model.Car;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class CarServiceImpl  implements CarService{
+    @Override
+    public List<Car> getCarsList(List<Car> cars, int number) {
+        return cars.stream().limit(number).collect(Collectors.toList());
+    }
+}
